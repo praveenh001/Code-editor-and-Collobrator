b1492806049a7e28c23ef4c2f4ab31f414000b8b
Praveen H <praveenpuni80@gmail.com>
2025-11-29 23:04:44 +0530

    Fixed refresh exiting bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // ⭐ REQUIRED FOR RENDER SPA ROUTING
-  publicDir: "public",
-
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  // copy _redirects manually into dist
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
+})
